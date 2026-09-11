c844003914a07e9f3985e3ab52e9babe13c64694
Onion3R <johnregis207@gmail.com>
2026-09-11 20:27:04 +0800

    refactor design for layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,15 @@ function Page() {
 
   const router = useRouter()
   return (
-    <div className="flex items-center justify-center h-screen">
-    <Button onClick={() => router.push('/compress')}>Image Compressor</Button>
-    <Button onClick={() => router.push('/pdf-organizer')}>PDF Tool</Button>
+    <div className="flex items-center justify-center h-screen gap-4">
+      <div className="flex flex-col gap-1 border  p-2 ">
+        <div className="h-40 w-50 bg-gray-200 rounded" />
+        <Button onClick={() => router.push('/compress')}>Image Compressor</Button>
+      </div>
+      <div className="flex flex-col gap-1 border  p-2 ">
+        <div className="h-40 w-50 bg-gray-200 rounded" />
+        <Button onClick={() => router.push('/pdf-organizer')}>PDF Tool</Button>
+      </div>
     </div>
   )
 }
